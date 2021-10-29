64bf0dce184fde1d98fa6e37a4d32000f88a1825
Caesar <caesar.hinlo.tech@gmail.com>
2021-10-29 13:37:57 +0800

    Implemented lifiting up state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,24 +2,31 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.counter.value,
     tag: ["tag1", "tag2", "tag3"],
   };
 
   handleIncrement = (product) => {
     console.log("[HINLO LOG]", product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
       <React.Fragment>
+        <h4>{this.props.counter.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement("Caesar")}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
         <ul>
           {this.state.tag.map((tag) => (
@@ -32,13 +39,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
