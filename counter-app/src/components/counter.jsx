d78ae97806ae32fc966f9f4a171d649595afc07c
Caesar <caesar.hinlo.tech@gmail.com>
2021-10-29 15:06:36 +0800

    Movie the state and methods tot the App component

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,23 +1,13 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-    tag: ["tag1", "tag2", "tag3"],
-  };
-
-  handleIncrement = (product) => {
-    console.log("[HINLO LOG]", product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <React.Fragment>
         <h4>{this.props.counter.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement("Caesar")}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -28,23 +18,18 @@ class Counter extends Component {
         >
           Delete
         </button>
-        <ul>
-          {this.state.tag.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
       </React.Fragment>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
